5c3a7396a106e3095d673b056d1585b81a3e2c84
Harmony <harmonised7@gmail.com>
2022-10-18 13:18:16 +0100

    Asynchronous camera updating and camera connection manager - automatic (re)connect

--- 4th_year_project/mainwindow.cpp
+++ 4th_year_project/mainwindow.cpp
@@ -6,7 +6,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    test();
+
+    connectCamera();
+    _frameUpdater.setInterval(_intervalMs);
+    #ifdef CONNECTION_MANAGER
+    _cameraConnectionManager.setInterval(1000);
+    if(!_vCap.isOpened())
+    {
+        qDebug() << "ERROR! Camera not found! Please connect the camera to continue.";
+        _frameUpdater.stop();
+        _cameraConnectionManager.start();
+    }
+    QObject::connect(&_cameraConnectionManager, &QTimer::timeout, this, &MainWindow::connectCamera);
+    #endif
+
+    _frameUpdater.start();
+    QObject::connect(&_frameUpdater, &QTimer::timeout, this, &MainWindow::onFrame);
 }
 
 MainWindow::~MainWindow()
@@ -14,49 +29,73 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-bool MainWindow::test()
+bool MainWindow::printDebug()
 {
-    vector<VideoCaptureAPIs> apis = videoio_registry::getBackends();
-    for(vector<VideoCaptureAPIs>::iterator iter = apis.begin()+1 ; iter != apis.end(); ++iter)
+//    vector<VideoCaptureAPIs> cameraApis = videoio_registry::getBackends();
+//    for(vector<VideoCaptureAPIs>::iterator iter = cameraApis.begin()+1 ; iter != cameraApis.end(); ++iter)
+//    {
+//        QString name = videoio_registry::getBackendName(*iter).c_str();
+//        qDebug() << name;
+//    }
+
+    vector<VideoCaptureAPIs> backendApis = videoio_registry::getBackends();
+    for(vector<VideoCaptureAPIs>::iterator iter = backendApis.begin()+1 ; iter != backendApis.end(); ++iter)
     {
         QString name = videoio_registry::getBackendName(*iter).c_str();
         qDebug() << name;
     }
-    Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap("/dev/video2", cv::CAP_V4L);
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-//    cap.open(deviceID, apiID);
-    // check if we succeeded
-    if (!cap.isOpened())
-    {
-        cerr << "ERROR! Unable to open camera\n";
-        return -1;
-    }
-    //--- GRAB AND WRITE LOOP
-    cout << "Start grabbing" << endl
-        << "Press any key to terminate" << endl;
-    for (;;)
-    {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
-        if (frame.empty()) {
-            cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
-        // show live and wait for a key with timeout long enough to show images
-        imshow("Live", frame);
-        if (waitKey(5) >= 0)
-            break;
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
+
+    qDebug() << _frameUpdater.isActive() << _frameUpdater.interval() << _framerate << _intervalMs;
+
     return 0;
+}
+
+void MainWindow::onFrame()
+{
+    ++_frameCount;
+    #ifdef FRAME_PRINT
+    qDebug() << "INFO: Retrieving frame..." << _frameCount;
+    #endif
+
+    // wait for a new frame from camera and store it into 'frame'
+    _vCap.read(_frame);
+    // check if we succeeded
+    if (_frame.empty())
+    {
+        qDebug() << "ERROR! Camera disconnected";
+        _frameUpdater.stop();
+        destroyWindow(_cameraWindowName);
+
+        #ifdef CONNECTION_MANAGER
+        _cameraConnectionManager.start();
+        _vCap.release();
+        #endif
+        return;
+    }
+    // show live and wait for a key with timeout long enough to show images
+    imshow(_cameraWindowName, _frame);
+}
+
+void MainWindow::connectCamera()
+{
+    #ifdef CONNECTION_PRINT
+    qDebug() << "INFO: Attempting connection..." << _vCap.isOpened();
+    #endif
+    if(!_vCap.isOpened())
+    {
+        _vCap.open(_cameraPath, _apiRef);
+        if(_vCap.isOpened())
+        {
+            qDebug() << "INFO: Camera connected";
+            _frameUpdater.start();
+
+            #ifdef CONNECTION_MANAGER
+            _cameraConnectionManager.stop();
+            #endif
+        }
+//        else
+//            _frameUpdater.stop();
+    }
 }
 
 
--- 4th_year_project/mainwindow.h
+++ 4th_year_project/mainwindow.h
@@ -2,14 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/videoio/registry.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/videoio/registry.hpp>
 
 #include <iostream>
 #include <stdio.h>
+
+//#define FRAME_PRINT
+#define CONNECTION_PRINT
+
+#define CONNECTION_MANAGER
 
 using namespace cv;
 using namespace std;
@@ -28,7 +34,23 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    int _framerate = 24;
+    int _intervalMs = 1000/_framerate;
+    int _frameCount = 0;
+    #ifdef CONNECTION_MANAGER
+    QTimer _cameraConnectionManager;
+    #endif
+    QTimer _frameUpdater;
 
-    bool test();
+    String _cameraPath = "/dev/video2";
+    String _cameraWindowName = "Live";
+    int _apiRef = cv::CAP_V4L;
+
+    VideoCapture _vCap;
+    Mat _frame;
+
+    bool printDebug();
+    void onFrame();
+    void connectCamera();
 };
 #endif // MAINWINDOW_H
